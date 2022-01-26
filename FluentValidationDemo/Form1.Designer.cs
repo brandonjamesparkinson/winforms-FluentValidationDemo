ad23ac262fdf593c03b72f8c61f478ab2babd80d
Brandon Parkinson <brandonjamesparkinson@gmail.com>
2022-01-26 23:58:21 +0000

    Add project files.

---
+++ FluentValidationDemo/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace FluentValidationDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCreateUser = new System.Windows.Forms.Label();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblAccountBalance = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblDateOfBirth = new System.Windows.Forms.Label();
+            this.errorListBox = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtAccountBalance = new System.Windows.Forms.TextBox();
+            this.lblErrorList = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.DTPDateOfBirth = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // lblCreateUser
+            // 
+            this.lblCreateUser.AutoSize = true;
+            this.lblCreateUser.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblCreateUser.Location = new System.Drawing.Point(12, 18);
+            this.lblCreateUser.Name = "lblCreateUser";
+            this.lblCreateUser.Size = new System.Drawing.Size(228, 54);
+            this.lblCreateUser.TabIndex = 0;
+            this.lblCreateUser.Text = "Create User";
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblFirstName.Location = new System.Drawing.Point(39, 100);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(106, 28);
+            this.lblFirstName.TabIndex = 1;
+            this.lblFirstName.Text = "First Name";
+            // 
+            // lblAccountBalance
+            // 
+            this.lblAccountBalance.AutoSize = true;
+            this.lblAccountBalance.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblAccountBalance.Location = new System.Drawing.Point(39, 181);
+            this.lblAccountBalance.Name = "lblAccountBalance";
+            this.lblAccountBalance.Size = new System.Drawing.Size(155, 28);
+            this.lblAccountBalance.TabIndex = 2;
+            this.lblAccountBalance.Text = "Account Balance";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblLastName.Location = new System.Drawing.Point(39, 141);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(103, 28);
+            this.lblLastName.TabIndex = 3;
+            this.lblLastName.Text = "Last Name";
+            // 
+            // lblDateOfBirth
+            // 
+            this.lblDateOfBirth.AutoSize = true;
+            this.lblDateOfBirth.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblDateOfBirth.Location = new System.Drawing.Point(39, 223);
+            this.lblDateOfBirth.Name = "lblDateOfBirth";
+            this.lblDateOfBirth.Size = new System.Drawing.Size(122, 28);
+            this.lblDateOfBirth.TabIndex = 4;
+            this.lblDateOfBirth.Text = "Date of Birth";
+            // 
+            // errorListBox
+            // 
+            this.errorListBox.Location = new System.Drawing.Point(460, 100);
+            this.errorListBox.Multiline = true;
+            this.errorListBox.Name = "errorListBox";
+            this.errorListBox.Size = new System.Drawing.Size(504, 178);
+            this.errorListBox.TabIndex = 5;
+            this.errorListBox.Text = "errorListBox";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(222, 100);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(160, 23);
+            this.txtFirstName.TabIndex = 6;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(222, 141);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(160, 23);
+            this.txtLastName.TabIndex = 7;
+            // 
+            // txtAccountBalance
+            // 
+            this.txtAccountBalance.Location = new System.Drawing.Point(222, 189);
+            this.txtAccountBalance.Name = "txtAccountBalance";
+            this.txtAccountBalance.Size = new System.Drawing.Size(160, 23);
+            this.txtAccountBalance.TabIndex = 8;
+            // 
+            // lblErrorList
+            // 
+            this.lblErrorList.AutoSize = true;
+            this.lblErrorList.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblErrorList.Location = new System.Drawing.Point(460, 69);
+            this.lblErrorList.Name = "lblErrorList";
+            this.lblErrorList.Size = new System.Drawing.Size(89, 28);
+            this.lblErrorList.TabIndex = 10;
+            this.lblErrorList.Text = "Error List";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCreate.Location = new System.Drawing.Point(222, 270);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(160, 49);
+            this.btnCreate.TabIndex = 11;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // DTPDateOfBirth
+            // 
+            this.DTPDateOfBirth.Location = new System.Drawing.Point(201, 228);
+            this.DTPDateOfBirth.Name = "DTPDateOfBirth";
+            this.DTPDateOfBirth.Size = new System.Drawing.Size(200, 23);
+            this.DTPDateOfBirth.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(976, 345);
+            this.Controls.Add(this.DTPDateOfBirth);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.lblErrorList);
+            this.Controls.Add(this.txtAccountBalance);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.errorListBox);
+            this.Controls.Add(this.lblDateOfBirth);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblAccountBalance);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.lblCreateUser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCreateUser;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblAccountBalance;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblDateOfBirth;
+        private System.Windows.Forms.TextBox errorListBox;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtAccountBalance;
+        private System.Windows.Forms.Label lblErrorList;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.DateTimePicker DTPDateOfBirth;
+    }
+}
---
+++ FluentValidationDemo/Form1.cs
@@ -0,0 +1,44 @@
+﻿using ModelLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FluentValidationDemo
+{
+    public partial class Form1 : Form
+    {
+        BindingList<string> errors = new BindingList<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            errorListBox.Clear();
+
+            if (!decimal.TryParse(txtAccountBalance.Text, out decimal accountBalance))
+            {
+                errors.Add("Account.Balance: Invalid Amount");
+                return;
+            }
+
+            PersonModel person = new PersonModel();
+            person.FirstName = txtFirstName.Text;
+            person.LastName = txtLastName.Text;
+            person.AccountBalance = decimal.Parse(txtAccountBalance.Text);
+            person.DateOfBirth = DTPDateOfBirth.Value;
+
+            // Insert into DB
+
+            MessageBox.Show("Operation Complete");
+        }
+    }
+}
